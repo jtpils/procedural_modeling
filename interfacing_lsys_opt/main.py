25e083af0e70064d0bfc025a2c452e521d172052
Like Gobeawan <gobeawanl@ihpc.a-star.edu.sg>
2018-03-07 13:25:28 +0800

    First commit test from Atom

---
+++ interfacing_lsys_opt/main.py
@@ -0,0 +1,19 @@
+# Created by Like on 7 Mar 2018
+# Last modified 7 Mar 2018
+
+def lsystem_run(age, no_1st_ord_branches, no_2nd_ord_branches, branching_angle_roll, branching_angle_pitch):
+    'Pass known parameter values into L-system rules to produce Lstring output'
+    pass #todo - Peng
+    return lstring_output
+
+def generate_lsystem_tree(age, no_1st_ord_branches, no_2nd_ord_branches, branching_angle_roll, branching_angle_pitch):
+    'Translate Lstring output into a list of points of cylinder base centers and their radii forming the trunk-branch representation of a tree'
+
+    out = lsystem_run(age, no_1st_ord_branches, no_2nd_ord_branches, branching_angle_roll, branching_angle_pitch)
+
+    #parse l-system output string
+    #todo
+    #to match with Xfrog object size
+
+
+    return output_point_list
