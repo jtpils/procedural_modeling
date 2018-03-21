eb7c4750ff93b7afa6257176fa162424c59569bd
deltayoung <deltayoung@users.noreply.github.com>
2018-03-21 15:38:44 +0800

    convert list to numpy.array + effort to hardcode with turtle graphics… (#1)
    * convert list to numpy.array + effort to hardcode with turtle graphics style
    
    * temp save
    
    * temp save, working up to age 4. need to fix branching (age 5 onwards).
    
    * minor variable rename

--- interfacing_lsys_opt/main.py
+++ interfacing_lsys_opt/main.py
@@ -1,5 +1,4 @@
 # Created by Like on 7 Mar 2018
-# Last modified 12 Mar 2018
 
 import numpy
 from pyquaternion import Quaternion
@@ -19,11 +18,11 @@ def lsystem_run(age, no_1st_ord_branches, no_2nd_ord_branches, branching_angle_r
 def generate_lsystem_tree(age, no_1st_ord_branches, no_2nd_ord_branches, branching_angle_roll, branching_angle_pitch):
     'Translate Lstring output into a list of points of cylinder base centers and their radii forming the trunk-branch representation of a tree'
 
+    current_post = numpy.array([0,0,0])
     if age == 0:
-        return [[0,0,0]]
+        return current_post
 
     out = lsystem_run(age, no_1st_ord_branches, no_2nd_ord_branches, branching_angle_roll, branching_angle_pitch)
-
     #parse l-system output string
     #todo
     #to match with Xfrog object size
@@ -32,22 +31,24 @@ def generate_lsystem_tree(age, no_1st_ord_branches, no_2nd_ord_branches, branchi
     # ========================
     output_point_list = [[0,0,0]]  #ground
     for t in range (1, age+1): #trunk
-        trunk_height = [0,0,2*t]
-        output_point_list = output_point_list + [trunk_height]
+        trunk = [0,0,2*t]
+        output_point_list = output_point_list + [trunk]
 
+    #branch
+    #note: consider current turtle orientation
     v = [0,0,1]
     pitch_axis = [1,0,0]
     roll_axis = [0,0,1]
     pitch_angle = branching_angle_pitch/180 * 3.14 #radian
     roll_angle = branching_angle_roll/180 * 3.14 #radian
 
-    if age > 2:
+    if age > 4:
         first_pitched_v = Quaternion(axis=pitch_axis,angle=pitch_angle).rotate(v)
         for t in range (0, no_1st_ord_branches): #1st order branch
             first_rolled_v = Quaternion(axis=roll_axis,angle=roll_angle).rotate(first_pitched_v)
-            output_point_list = output_point_list + [[a+b for a,b in zip(trunk_height,first_rolled_v)]]
+            output_point_list = output_point_list + [[a+b for a,b in zip(trunk,first_rolled_v)]]
 
-            if age > 3:
+            if age > 5:
                 new_pitch_axis = Quaternion(axis=roll_axis,angle=roll_angle).rotate(pitch_axis)
                 new_roll_axis = first_rolled_v
                 second_pitched_v = Quaternion(axis=new_pitch_axis,angle=pitch_angle).rotate([0.5*x for x in first_rolled_v])
@@ -87,7 +88,7 @@ def optimise():
     return growth_param_list
 
 def main():
-    age = 4
+    age = 5
     no_1st_ord_branches = 2
     no_2nd_ord_branches = 3
     branching_angle_roll = 30
