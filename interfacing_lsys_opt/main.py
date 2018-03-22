ba897c35f0a0de32575e191d204fad08258b1f36
kayu cawan <kayu.cawan@gmail.com>
2018-03-23 01:26:44 +0800

    completed debugging. code done for hardwiring tree growth up to 2nd order branch. parameters: age, number of 1st order branches, number of 2nd order branches, branching angle (roll & pitch)

--- interfacing_lsys_opt/main.py
+++ interfacing_lsys_opt/main.py
@@ -42,54 +42,61 @@ def generate_lsystem_tree(age, no_1st_ord_branches, no_2nd_ord_branches, branchi
 
     output_point_list = [current_post]  #ground
     for t in range (1, age+1): # growing trunk
-        current_post = current_post + numpy.array([0.0, 0.0, trunk_growth_rate])   #do not use operator +=, as it overwrites all values into one current_post memory location
+        current_post = current_post + current_dir*trunk_growth_rate   #do not use operator +=, as it overwrites all values into one current_post memory location
         output_point_list += [current_post]
 
-    #push state into stack
-    branching_stack.append([current_post, current_dir, current_up, current_left])
-    print branching_stack
-
     if age > 4: #1st order branches start growing
-        pitch = Quaternion(axis=current_left,angle=pitch_angle)
-        roll = Quaternion(axis=current_dir,angle=roll_angle)
-        rotation = pitch
+        #push state into stack
+        branching_stack.append([current_post, current_dir, current_up, current_left])
+        #print branching_stack
+
+        pitch1 = Quaternion(axis=current_left,angle=pitch_angle)
+        roll1 = Quaternion(axis=current_dir,angle=roll_angle)
+        current_dir = pitch1.rotate(current_dir)
+        current_up = pitch1.rotate(current_up)
+        current_left = pitch1.rotate(current_left)
+        first_branching_node = current_post
 
         for s in range (0, no_1st_ord_branches): #1st order branches
-            rotation = rotation*roll   #pitch, then roll
-            current_dir = rotation.rotate(current_dir)
-            current_up = rotation.rotate(current_up)
-            current_left = rotation.rotate(current_left)
+            current_dir = roll1.rotate(current_dir)
+            current_up = roll1.rotate(current_up)
+            current_left = roll1.rotate(current_left)
 
             for t in range(age-4): #growing 1st order branch
-                current_post = current_post + current_dir*branch_growth_rate
+                current_post = first_branching_node + (t+1)*current_dir*branch_growth_rate
                 output_point_list += [current_post]
 
-            #push state into stack
-            branching_stack.append([current_post, current_dir, current_up, current_left])
-            print branching_stack
-
             if age > 5: #2nd order branches start growing
-                pitch = Quaternion(axis=current_left,angle=pitch_angle)
-                roll = Quaternion(axis=current_dir,angle=roll_angle)
-                rotation = pitch
+                #push state into stack
+                print("2nd order - Current_post=%s" % (current_post))
+                branching_stack.append([current_post, current_dir, current_up, current_left])
+                #print branching_stack
+
+                pitch2 = Quaternion(axis=current_left,angle=pitch_angle)
+                roll2 = Quaternion(axis=current_dir,angle=roll_angle)
+                current_dir = pitch2.rotate(current_dir)
+                current_up = pitch2.rotate(current_up)
+                current_left = pitch2.rotate(current_left)
+                second_branching_node = current_post
 
                 for u in range (0, no_2nd_ord_branches): #2nd order branch
-                    rotation = rotation*roll   #pitch, then roll
-                    current_dir = rotation.rotate(current_dir)
-                    current_up = rotation.rotate(current_up)
-                    current_left = rotation.rotate(current_left)
+                    current_dir = roll2.rotate(current_dir)
+                    current_up = roll2.rotate(current_up)
+                    current_left = roll2.rotate(current_left)
 
                     for v in range(age-5): #growing 2nd order branch
-                        current_post = current_post + current_dir*branch_growth_rate
+                        current_post = second_branching_node + (v+1)*current_dir*branch_growth_rate
                         output_point_list += [current_post]
 
-            #pop stack to previous state
-            [current_post, current_dir, current_up, current_left] = branching_stack.pop()
-            print branching_stack
+                #pop stack to previous state
+                [current_post, current_dir, current_up, current_left] = branching_stack.pop()
+                #print branching_stack
+
+
 
         #pop stack to previous state
         [current_post, current_dir, current_up, current_left] = branching_stack.pop()
-        print branching_stack
+        #print branching_stack
 
     # ===========================================
 
@@ -120,11 +127,11 @@ def optimise():
     return growth_param_list
 
 def main():
-    print generate_lsystem_tree(age=6,
+    print generate_lsystem_tree(age=10,
                                 no_1st_ord_branches=2,
                                 no_2nd_ord_branches=3,
                                 branching_angle_roll=30.0,
-                                branching_angle_pitch=21.0
+                                branching_angle_pitch=20.0
                                 )
 
 if __name__ == "__main__":
