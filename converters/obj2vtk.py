e164481ee6c43b776352cb0f5aa5788f230608ce
Like Gobeawan <gobeawanl@ihpc.a-star.edu.sg>
2018-03-29 18:27:05 +0800

    completed obj2vtk script

--- converters/obj2vtk.py
+++ converters/obj2vtk.py
@@ -4,22 +4,27 @@
 
 import os
 
-path_name = "C:/Users/gobeawanl/Documents/VS1 Tree Modelling/Work/Daniel/20180328 Optimisation 5 params 2000 init conds 122min runtime/vtks/"
-file_names = [file_name in os.listdir(path_name) if os.path.isfile(os.path.join(path_name, file_name))]
+input_path = "C:/Users/gobeawanl/Documents/VS1 Tree Modelling/Work/Daniel/20180328 Optimisation 5 params 2000 init conds 122min runtime/objs/"
+output_path = "C:/Users/gobeawanl/Documents/VS1 Tree Modelling/Work/Daniel/20180328 Optimisation 5 params 2000 init conds 122min runtime/vtks/"
+file_names = [file_name for file_name in os.listdir(input_path) if os.path.isfile(os.path.join(input_path, file_name))]
 vtktype = "POLYDATA"    #type of data in VTK
 datatype = "integer"    #datatype of numbers in VTK
 
+def find_no_points(file_name):
+    with open(file_name) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
+
 for file_name in file_names:
-    file fi = open(file_name, "r")
+    fi = open(os.path.join(input_path, file_name), "r")
     file_out = file_name.rsplit('.', 1)
-    file fo = open(file_out[0]+".vtk", "w")
+    fo = open(os.path.join(output_path, file_out[0]+".vtk"), "w")
 
-    no_lines =
-
-    fo.write("# vtk DataFile Version 3.0\nTree points\nASCII\nDATASET "+vtktype+"\n\nPOINTS "+no_lines+" "+datatype+" ")
-    for :
-        fi.read()
-        fo.write("%s")
+    no_points = find_no_points(os.path.join(input_path, file_name))
+    fo.write("# vtk DataFile Version 3.0\nTree points\nASCII\nDATASET "+vtktype+"\n\nPOINTS "+str(no_points)+" "+datatype+" ")
+    for aPoint in fi:
+        fo.write("%s" % aPoint[2:])
 
     fi.close()
     fo.close()
