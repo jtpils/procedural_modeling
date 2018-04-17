9a63bf2da4a745053a15dde15c39d9e16920280e
Like Gobeawan <gobeawanl@ihpc.a-star.edu.sg>
2018-04-17 17:57:20 +0800

    Structuring the repository

--- interfacing_lsys_opt/lpy2mtg2pts.py
+++ interfacing_lsys_opt/lpy2mtg2pts.py
@@ -5,12 +5,11 @@ output_file = os.path.join(current_path, 'example_output.png')
 mtg_file = os.path.join(current_path, 'lstring_output.mtg')
 
 import openalea.lpy as lpy
-from openalea.lpy import AxialTree
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 import time
 from openalea.plantgl.all import *
-from openalea.mtg.io import lpy2mtg, mtg2lpy, axialtree2mtg, mtg2axialtree, read_lsystem_string
+from openalea.mtg.io import lpy2mtg, mtg2lpy, axialtree2mtg, mtg2axialtree
 from openalea.mtg.aml import *
 
 l = lpy.Lsystem(input_file)
@@ -21,16 +20,11 @@ l.plot(axialtree)
 
 Viewer.frameGL.saveImage(output_file, 'png')
 
-scale = {'F':1,'X':1}
-#mtg = lpy2mtg(axialtree, l)
-#mtg_lines = lpy2mtg(mtg, axialtree, l)
-#mtg_lines = write_mtg(mtg)
-
-mtg = read_lsystem_string(str(axialtree), scale)
+mtg = lpy2mtg(axialtree, l)
+mtg_lines = write_mtg(mtg)
 
 f = open(mtg_file, 'w')
-#f.write(mtg_lines)
-f.write(str(mtg))
+f.write(mtg_lines)
 
 #use dict to access mtg structure....
 
