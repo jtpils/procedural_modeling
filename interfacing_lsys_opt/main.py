1977b02c1e0774290eb9b3515775111d07c87161
Like Gobeawan <gobeawanl@ihpc.a-star.edu.sg>
2018-05-02 18:14:59 +0800

    rename function generate_lsystem_tree_points(age, nb1, nb2, roll, pitch) to ngenerate_lsystem_tree_points to avoid overloading conflict with the same name function with <params> array input

--- interfacing_lsys_opt/main.py
+++ interfacing_lsys_opt/main.py
@@ -23,23 +23,15 @@ def parse_lstring(lstring):
     return output_point_list
 
 def generate_lsystem_tree_points(params):
-    'Parse L-System string output into a list of points of cylinder base centers and their radii forming the trunk-branch skeleton representation of a tree'
-    age = int(params[0])
-    no_1st_ord_branches=int(params[1])
-    no_2nd_ord_branches=int(params[2])
-    branching_angle_roll=params[3]
-    branching_angle_pitch=params[4]
     'Wrapper function to map params array to individual variables'
 
-    age = params[0]
-    no_1st_ord_branches=params[1]
-    no_2nd_ord_branches=params[2]
-    branching_angle_roll=params[3]
-    branching_angle_pitch=params[4]
+    return ngenerate_lsystem_tree_points(   age=int(params[0]),
+                                            no_1st_ord_branches=int(params[1]),
+                                            no_2nd_ord_branches=int(params[2]),
+                                            branching_angle_roll=params[3],
+                                            branching_angle_pitch=params[4] )
 
-    return generate_lsystem_tree_points(age, no_1st_ord_branches, no_2nd_ord_branches, branching_angle_roll, branching_angle_pitch)
-
-def generate_lsystem_tree_points(age, no_1st_ord_branches, no_2nd_ord_branches, branching_angle_roll, branching_angle_pitch):
+def ngenerate_lsystem_tree_points(age, no_1st_ord_branches, no_2nd_ord_branches, branching_angle_roll, branching_angle_pitch):
     'Parse L-System string output into a list of points of cylinder base centers and their radii forming the trunk-branch skeleton representation of a tree'
 
     lstring = lsystem_run(age, no_1st_ord_branches, no_2nd_ord_branches, branching_angle_roll, branching_angle_pitch)
@@ -149,7 +141,7 @@ def optimise():
     return growth_param_list
 
 def main():
-    output = generate_lsystem_tree_points(numpy.asarray([20,5,8,75.0,45.0]))
+    output = generate_lsystem_tree_points(numpy.array([20,5,8,75.0,45.0]))
     print output
     file = open('output.obj', 'w')
     for item in output:
