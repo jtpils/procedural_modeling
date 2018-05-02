79aec5a42b866119e4846494abe247bce1e195ab
Daniel Wise <djw135@gmail.com>
2018-05-02 10:07:50 +0800

    Edits to optimisation routine

--- optimisation/opt.py
+++ optimisation/opt.py
@@ -5,9 +5,97 @@ import scipy.optimize as optimize
 from numpy.linalg import eig, inv
 from pyquaternion import Quaternion
 from os.path import isfile
+import glob
 #import sys
 #ys.path.append('../interfacing_lysys_opt')
 
+def generate_lsystem_tree_points(p):
+	'Parse L-System string output into a list of points of cylinder base centers and their radii forming the trunk-branch skeleton representation of a tree'
+	age = int(60*p[0]);
+	no_1st_ord_branches = int(10*p[1]);
+	no_2nd_ord_branches = int(10*p[2]);
+	branching_angle_roll = 180*p[3];
+	branching_angle_pitch = 10+80*p[4];
+    #lstring = lsystem_run(age, no_1st_ord_branches, no_2nd_ord_branches, branching_angle_roll, branching_angle_pitch)
+    #parse l-system output string into 3d points matching Xfrog object size
+    #todo
+
+    # temporary hardcoding of tree growth: age 1-2 trunk grows taller by 2 m/yr, age 3 first branch out by 1 m/yr, age 4 second branch out by 0.5 m/yr
+    # ========================
+	trunk_growth_rate = 2.0
+	branch_growth_rate = 1.0
+	pitch_angle = branching_angle_pitch/180.0 * 3.14159 #radian
+	roll_angle = branching_angle_roll/180.0 * 3.14159 #radian
+
+	current_post = numpy.array([0.0, 0.0, 0.0]) #tracking turtle's position
+	current_dir = numpy.array([0.0, 0.0, 1.0])  #tracking turtle's orientation/heading (vector H)
+	current_up = numpy.array([0.0, 1.0, 0.0])   #tracking turtle's up (vector U)
+	current_left = numpy.array([1.0, 0.0, 0.0]) #tracking turtle's left (vector L)
+
+	branching_stack = []
+
+	if age == 0:
+			return [current_post]
+
+	output_point_list = [current_post]  #ground
+	for t in range (1, age+1): # growing trunk
+		current_post = current_post + current_dir*trunk_growth_rate   #do not use operator +=, as it overwrites all values into one current_post memory location
+		output_point_list += [current_post]
+
+	if age > 4: #1st order branches start growing
+        #push state into stack
+		branching_stack.append([current_post, current_dir, current_up, current_left])
+        #print branching_stack
+
+		pitch1 = Quaternion(axis=current_left,angle=pitch_angle)
+		roll1 = Quaternion(axis=current_dir,angle=roll_angle)
+		current_dir = pitch1.rotate(current_dir)
+		current_up = pitch1.rotate(current_up)
+		current_left = pitch1.rotate(current_left)
+		first_branching_node = current_post
+
+        for s in range (0, no_1st_ord_branches): #1st order branches
+            current_dir = roll1.rotate(current_dir)
+            current_up = roll1.rotate(current_up)
+            current_left = roll1.rotate(current_left)
+
+            for t in range(age-4): #growing 1st order branch
+                current_post = first_branching_node + (t+1)*current_dir*branch_growth_rate
+                output_point_list += [current_post]
+
+            if age > 5: #2nd order branches start growing
+                #push state into stack
+                #print("2nd order - Current_post=%s" % (current_post))
+                branching_stack.append([current_post, current_dir, current_up, current_left])
+                #print branching_stack
+
+                pitch2 = Quaternion(axis=current_left,angle=pitch_angle)
+                roll2 = Quaternion(axis=current_dir,angle=roll_angle)
+                current_dir = pitch2.rotate(current_dir)
+                current_up = pitch2.rotate(current_up)
+                current_left = pitch2.rotate(current_left)
+                second_branching_node = current_post
+
+                for u in range (0, no_2nd_ord_branches): #2nd order branch
+                    current_dir = roll2.rotate(current_dir)
+                    current_up = roll2.rotate(current_up)
+                    current_left = roll2.rotate(current_left)
+
+                    for v in range(age-5): #growing 2nd order branch
+                        current_post = second_branching_node + (v+1)*current_dir*branch_growth_rate
+                        output_point_list += [current_post]
+
+                #pop stack to previous state
+                [current_post, current_dir, current_up, current_left] = branching_stack.pop()
+                #print branching_stack
+
+        #pop stack to previous state
+        [current_post, current_dir, current_up, current_left] = branching_stack.pop()
+        #print branching_stack
+
+    # ===========================================
+
+	return output_point_list
 
 def fit_ellipse(x,y):
 # Fit_ellipse function takes in 2D points in x,y (i.e. 1D arrays)
@@ -100,10 +188,10 @@ def calc_growth_space_vx(pts):
 	eu = numpy.array([u_et1,u_e1,u_e2])
 	return bbox, trunk_height, crown_height, mu_c, el, eu#bbox,trunk_height,crown_height,mu_c,el,eu
 
-def load_target_points():
+#def load_target_points():
 	# Function loads target_points from obj file
-    tpts = numpy.genfromtxt(target_filename,delimiter=' ',usecols=(1,2,3))
-    return tpts
+    #tpts = numpy.genfromtxt(target_filename,delimiter=' ',usecols=(1,2,3))
+    #return tpts
 
 def load_normalised_voxel_growth_space():
 	# Function reads growth_space file, shifts x,y coordinates
@@ -114,30 +202,37 @@ def load_normalised_voxel_growth_space():
 	gs[:,2] = gs[:,2] - numpy.min(gs[:,2])
 	return gs
 
+# Global variable
+target_filename='../../obj_files/target.obj'
+gs_filename='./growth_space.csv'
+
+tpts = load_normalised_voxel_growth_space()
+
 def estimate_error(params):
 # Estimate_error function takes in:
 # params: parameters used to create tree from L-System
 # fname: filename of obj file (comparison data)
-    tpts = load_normalised_voxel_growth_space()
-    targ_bbox,targ_th,targ_ch,targ_mu,targ_el,targ_eu = calc_growth_space_vx(tpts)
+	tpts = load_normalised_voxel_growth_space()
+	targ_bbox,targ_th,targ_ch,targ_mu,targ_el,targ_eu = calc_growth_space_vx(tpts)
 	# Generate L-system points for params
-    ls_pts = generate_lsystem_tree_points(params)
-    ls_pts = numpy.asarray(ls_pts)
-    ls_bbox, ls_th, ls_ch, ls_mu, ls_el, ls_eu = calc_growth_space_ls(ls_pts)
+	ls_pts = generate_lsystem_tree_points(params)
+	ls_pts = numpy.asarray(ls_pts)
+	ls_bbox, ls_th, ls_ch, ls_mu, ls_el, ls_eu = calc_growth_space_ls(ls_pts)
 	# Calculate error
-    Ebbox = numpy.sqrt((((ls_bbox[1]-ls_bbox[0])-(targ_bbox[1]-targ_bbox[0]))/(targ_bbox[1]-targ_bbox[0]))**2 + \
-			(((ls_bbox[3]-ls_bbox[2])-(targ_bbox[3]-targ_bbox[2]))/(targ_bbox[3]-targ_bbox[2]))**2) #BBox error
-    Eth = numpy.sqrt(((ls_th-targ_th)/targ_th)**2) # Trunk height error
-    Ech = numpy.sqrt(((ls_ch-targ_ch)/targ_ch)**2) # Crown height error
+	Ebbox = numpy.sqrt((((ls_bbox[1]-ls_bbox[0])-(targ_bbox[1]-targ_bbox[0]))/(targ_bbox[1]-targ_bbox[0]))**2 + \
+		(((ls_bbox[3]-ls_bbox[2])-(targ_bbox[3]-targ_bbox[2]))/(targ_bbox[3]-targ_bbox[2]))**2) #BBox error
+	Eth = numpy.sqrt(((ls_th-targ_th)/targ_th)**2) # Trunk height error
+	Ech = numpy.sqrt(((ls_ch-targ_ch)/targ_ch)**2) # Crown height error
 	#Eel_theta = (ls_el[0]-targ_el[0])/(2*numpy.pi)
-    Eel_radii = numpy.mean(((ls_el[1:2]-targ_el[1:2])/(targ_el[1:2]))**2)
+	Eel_radii = numpy.mean(((ls_el[1:2]-targ_el[1:2])/(targ_el[1:2]))**2)
 	#Eeu_theta = (ls_eu[0]-targ_eu[0])/(2*numpy.pi)
-    Eeu_radii = numpy.mean(((ls_eu[1:2]-targ_eu[1:2])/(targ_eu[1:2]))**2)
-    E_total = numpy.mean(numpy.array(\
+	Eeu_radii = numpy.mean(((ls_eu[1:2]-targ_eu[1:2])/(targ_eu[1:2]))**2)
+	E_total = numpy.mean(numpy.array(\
 		[Eth,Ech,Eel_radii,Eeu_radii]))
-    return E_total
+	#print params, E_total
+	return E_total
 
-def optimise(npts,params):
+def optimise(npts,nparams):
 # Optimise function takes in:
 # npts:     number of sample points for Latin Hypercube sampling
 # params:   list of parameters to be optimised (range 0:1)
@@ -145,116 +240,27 @@ def optimise(npts,params):
 # result: list of optimum values obtained from each initial condition
 # error according to 'result'
     result=[];error=[];
-    np = len(params)
-    sample_points = pyDOE.lhs(np,samples=np*npts,criterion='center')
-    for i in range(npts):
-        p = sample_points[i,:]
-        try:
-            opt_params=optimize.minimize(estimate_error,p,method='Nelder-Mead')
-            result.append(opt_params.x); error.append(opt_params.fun)
-        except:
-            pass
+    sample_points = pyDOE.lhs(nparams,samples=nparams*npts,criterion='center')
+    for i in range(len(sample_points)):
+		p = sample_points[i,:]
+		#print p
+		try:
+			print "Trying point", i
+			opt_params=optimize.minimize(estimate_error,p,method='Nelder-Mead')
+			result.append(opt_params.x); error.append(opt_params.fun)
+			print error
+		except:
+			pass
     return result,error
-
-# Global variable
-target_filename='../../obj_files/target.obj'
-gs_filename='./growth_space.csv'
-
-def generate_lsystem_tree_points(params):
-	'Parse L-System string output into a list of points of cylinder base centers and their radii forming the trunk-branch skeleton representation of a tree'
-	age = params[0]
-	no_1st_ord_branches=params[1]
-	no_2nd_ord_branches=params[2]
-	branching_angle_roll=params[3]
-	branching_angle_pitch=params[4]
-    #lstring = lsystem_run(age, no_1st_ord_branches, no_2nd_ord_branches, branching_angle_roll, branching_angle_pitch)
-    # return parse_lstring(lstring)
-    #parse l-system output string into 3d points matching Xfrog object size
-    #todo
-
-    # temporary hardcoding of tree growth: age 1-2 trunk grows taller by 2 m/yr, age 3 first branch out by 1 m/yr, age 4 second branch out by 0.5 m/yr
-    # ========================
-	trunk_growth_rate = 2.0
-	branch_growth_rate = 1.0
-	pitch_angle = branching_angle_pitch/180.0 * 3.14159 #radian
-	roll_angle = branching_angle_roll/180.0 * 3.14159 #radian
-
-	current_post = numpy.array([0.0, 0.0, 0.0]) #tracking turtle's position
-	current_dir = numpy.array([0.0, 0.0, 1.0])  #tracking turtle's orientation/heading (vector H)
-	current_up = numpy.array([0.0, 1.0, 0.0])   #tracking turtle's up (vector U)
-	current_left = numpy.array([1.0, 0.0, 0.0]) #tracking turtle's left (vector L)
-
-	branching_stack = []
-
-	if age == 0:
-			return [current_post]
-
-	output_point_list = [current_post]  #ground
-	for t in range (1, age+1): # growing trunk
-		current_post = current_post + current_dir*trunk_growth_rate   #do not use operator +=, as it overwrites all values into one current_post memory location
-		output_point_list += [current_post]
-
-	if age > 4: #1st order branches start growing
-        #push state into stack
-		branching_stack.append([current_post, current_dir, current_up, current_left])
-        #print branching_stack
-
-		pitch1 = Quaternion(axis=current_left,angle=pitch_angle)
-		roll1 = Quaternion(axis=current_dir,angle=roll_angle)
-		current_dir = pitch1.rotate(current_dir)
-		current_up = pitch1.rotate(current_up)
-		current_left = pitch1.rotate(current_left)
-		first_branching_node = current_post
-
-        for s in range (0, no_1st_ord_branches): #1st order branches
-            current_dir = roll1.rotate(current_dir)
-            current_up = roll1.rotate(current_up)
-            current_left = roll1.rotate(current_left)
-
-            for t in range(age-4): #growing 1st order branch
-                current_post = first_branching_node + (t+1)*current_dir*branch_growth_rate
-                output_point_list += [current_post]
-
-            if age > 5: #2nd order branches start growing
-                #push state into stack
-                #print("2nd order - Current_post=%s" % (current_post))
-                branching_stack.append([current_post, current_dir, current_up, current_left])
-                #print branching_stack
-
-                pitch2 = Quaternion(axis=current_left,angle=pitch_angle)
-                roll2 = Quaternion(axis=current_dir,angle=roll_angle)
-                current_dir = pitch2.rotate(current_dir)
-                current_up = pitch2.rotate(current_up)
-                current_left = pitch2.rotate(current_left)
-                second_branching_node = current_post
-
-                for u in range (0, no_2nd_ord_branches): #2nd order branch
-                    current_dir = roll2.rotate(current_dir)
-                    current_up = roll2.rotate(current_up)
-                    current_left = roll2.rotate(current_left)
-
-                    for v in range(age-5): #growing 2nd order branch
-                        current_post = second_branching_node + (v+1)*current_dir*branch_growth_rate
-                        output_point_list += [current_post]
-
-                #pop stack to previous state
-                [current_post, current_dir, current_up, current_left] = branching_stack.pop()
-                #print branching_stack
-
-        #pop stack to previous state
-        [current_post, current_dir, current_up, current_left] = branching_stack.pop()
-        #print branching_stack
-
-    # ===========================================
-
-	return output_point_list
 
 def main():
 	print "Running main function of opt.py..."
-	print "Loading growth space file"
-	#ET = estimate_error(numpy.asarray([20,5,8,75,25]))
-	load_normalised_voxel_growth_space()
-	#print ET
+	result,error = optimise(1000,5)
+	result = numpy.asarray(result)
+	error = numpy.asarray(numpy.abs(error))
+	loc = numpy.where(error == error.min())
+	print result[loc,:], error.min()
+	numpy.save('results.npy',result); numpy.save('errors.npy',error);
 
 
 if __name__ == "__main__":
