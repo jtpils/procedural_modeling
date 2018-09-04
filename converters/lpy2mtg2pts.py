5a554761697f886755371dfbd2e744a57d20f0df
Like_IHPC <deltayoung@gmail.com>
2018-09-04 16:18:03 +0800

    note: copy converters/io.py into equiv loc of 'C:\Python27\Lib\site-packages\OpenAlea.Mtg-1.2.0-py2.7.egg\openalea\mtg' to overwrite default mtg writing function

--- converters/lpy2mtg2pts.py
+++ converters/lpy2mtg2pts.py
@@ -1,3 +1,5 @@
+#Note: to copy file 'io.py' in this folder to the equivalent location of 'C:\Python27\Lib\site-packages\OpenAlea.Mtg-1.2.0-py2.7.egg\openalea\mtg' (overwrite existing io.py to modify mtg writing)
+
 import os
 import time
 from PyQt4.QtCore import *
