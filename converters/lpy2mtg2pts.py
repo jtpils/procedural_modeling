7360e2999d4a12ac699f7a38283ccc49a61640a9
Like Gobeawan <gobeawanl@ihpc.a-star.edu.sg>
2019-03-27 15:30:03 +0800

    slight change of params for vs2 update

--- converters/lpy2mtg2pts.py
+++ converters/lpy2mtg2pts.py
@@ -159,7 +159,7 @@ def test_xmlgrowthspace2obj():
 
 
 if __name__ == "__main__":
-    lsystem_run(species=Species.PP, age=35,
+    lsystem_run(species=Species.SP, age=35,
                 trunk_pitch_angle=0.64, trunk_roll_angle=-0.47, trunk_height=2.03,
                 no_first_ord_branches=1,
                 branching_pitch_angle=76.95, branching_roll_angle=148.02,
